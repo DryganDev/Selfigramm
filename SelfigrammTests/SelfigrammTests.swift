3051f0ab03e9a031cbd2c86cfed685c73b87acd6
Artsiom Sazonau <gigo12121212@gmail.com>
2017-02-16 20:40:43 +0300

    Initial Commit

---
+++ SelfigrammTests/SelfigrammTests.swift
@@ -0,0 +1,36 @@
+//
+//  SelfigrammTests.swift
+//  SelfigrammTests
+//
+//  Created by Artsiom Sazonau on 16.02.17.
+//  Copyright © 2017 Arts Corp. All rights reserved.
+//
+
+import XCTest
+@testable import Selfigramm
+
+class SelfigrammTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
